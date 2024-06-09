f6420f4b761d1537a6c67f5bca1ddbe7203600d5
Dawei Hao <dwsamurai84@gmail.com>
2024-06-10 00:30:36 +0800

    Initial Commit

---
+++ About Me/About_MeApp.swift
@@ -0,0 +1,17 @@
+//
+//  About_MeApp.swift
+//  About Me
+//
+//  Created by Dawei Hao on 2024/6/10.
+//
+
+import SwiftUI
+
+@main
+struct About_MeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ About Me/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  About Me
+//
+//  Created by Dawei Hao on 2024/6/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
