ee9dd5e47f29fa54cc7ed633a702f8cc7e1194ec
Dawei Hao <dwsamurai84@gmail.com>
2024-06-11 00:56:52 +0800

    To show ny personal hobbies, foods, colors.

--- About Me/FavoritesView.swift
+++ About Me/FavoritesView.swift
@@ -9,7 +9,49 @@ import SwiftUI
 
 struct FavoritesView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Favorites")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding(.bottom, 40)
+            
+            // HStack for hobbies
+            Text("Hobbies")
+                .font(.title2)
+            
+            HStack {
+                ForEach(information.hobbies, id: \.self) { hobby in
+                    Image(systemName: hobby)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(maxWidth: 80, maxHeight: 60)
+                }
+                .padding()
+            }
+            .padding()
+            
+            // HStack for Foods
+            Text("Foods")
+                .font(.title2)
+            
+            HStack(spacing: 60) {
+                ForEach(information.foods, id: \.self) { food in
+                    Text(food)
+                        .font(.system(size: 48))
+                }
+            }
+            .padding()
+            
+            // HStack for color
+            HStack(spacing: 30) {
+                ForEach(information.colors, id: \.self) { Color in
+                    Color
+                        .frame(width: 70, height: 70)
+                        .cornerRadius(10)
+                }
+            }
+            .padding()
+        }
     }
 }
 
