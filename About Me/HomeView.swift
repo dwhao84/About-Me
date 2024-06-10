7089be00bd5c125490544bdd9656382500161b8a
Dawei Hao <dwsamurai84@gmail.com>
2024-06-11 00:58:20 +0800

    Show the my personal name and porfile.

--- About Me/HomeView.swift
+++ About Me/HomeView.swift
@@ -8,8 +8,23 @@
 import SwiftUI
 
 struct HomeView: View {
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("All About")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding()
+            
+            Image(information.image)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .cornerRadius(150)
+                .padding(40)
+            
+            Text(information.name)
+                .font(.title)
+        }
     }
 }
 
